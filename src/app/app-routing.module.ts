24c9d4f590087e9de34cb70e676db6898637f2a6
Maria <mb.lev17@gmail.com>
2020-10-24 10:19:46 -0500

    Defined component in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'edit-profile', component: EditProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
