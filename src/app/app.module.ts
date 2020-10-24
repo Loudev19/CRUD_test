271dff084d9fc01b6a3a8520f83f5c191015e8bd
Maria <mb.lev17@gmail.com>
2020-10-24 14:13:18 -0500

    Load auto components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthService } from "./auth.service";
+import { TaskService } from "./task.service";
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    TasksComponent,
+    NavbarComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
